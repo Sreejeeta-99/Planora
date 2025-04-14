216a20b56a5d9e73a84efe0631cb0ea0c82e3bb3
Sreejeeta-99 <sreejeetadutta000@gmail.com>
2025-04-14 12:54:29 +0530

    DAY 05

--- controller/blog.js
+++ controller/blog.js
@@ -15,3 +15,48 @@ export const createBlog = async (req, res) => {
   await userExist.save();
   return res.json({ message: "Blog created successfully", blog: blog._id });
 };
+
+// Fetching all blogs from the database
+export const getAllBlogs = async(req,res) =>{
+  const blogs = await Blog.find().populate('author', null, null, { strictPopulate:false}).sort({createdAt:-1});
+  return res.json(blogs);
+}
+
+//Fetching a single blog by its ID
+export const fetchSingleBlog = async(req,res) =>{
+  const blog = await Blog.findById(req.params.id).populate('author', null, null, { strictPopulate: false });
+  if(!blog){
+    return res.status(404).json({error: "Blog not found"});
+  }
+    return res.json(blog);
+}
+
+//update blog
+export const updateBlog = async (req, res) => {
+  const blog = await Blog.findById(req.params.id);
+  if (!blog) {
+    return res.status(404).json({ error: "Blog not found" });
+  }
+  const { title, content} = req.body;
+  blog.title = title;
+  blog.content = content;
+  await blog.save();
+  return res.json({ message: "Blog updated successfully", blog });
+}
+
+//Delete blog
+export const deleteBlog = async (req, res) => {
+  const blog = await Blog.findById(req.params.id).populate("author", null, null, { strictPopulate: false });
+  console.log(blog);
+  if (!blog) {
+    return res.status(404).json({ error: "Blog not found" });
+  }
+  if (!blog.author) {
+    return res.status(404).json({ error: "Blog author not found" });
+  }
+  if (blog.author.toString() !== req.userId) {
+    return res.status(403).json({ error: "Unauthorized" });
+  }  
+  await Blog.findByIdAndDelete(req.params.id);
+  return res.json({ message: "Blog deleted successfully" });
+}
--- routes/blog.js
+++ routes/blog.js
@@ -1,7 +1,11 @@
 import express from "express";
-import { createBlog } from "../controller/blog.js";
+import { createBlog, fetchSingleBlog, getAllBlogs, updateBlog, deleteBlog} from "../controller/blog.js";
 import { authenticate_token } from "../middlewares/auth.js";
 
 const router = express.Router();
-router.post("/create",authenticate_token,createBlog);
+router.post("/create", authenticate_token, createBlog);
+router.get("/fetch/",getAllBlogs);
+router.get("/fetch/:id", fetchSingleBlog);
+router.put("/update/:id", authenticate_token, updateBlog);
+router.delete("/delete/:id",authenticate_token, deleteBlog);
 export default router;
